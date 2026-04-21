e760e58091db12ec0964832a3bb5801c1b87fb55
SanyamRajSingh <sanyamrajsingh@gmail.com>
2026-04-21 21:21:08 +0530

    UC3: Refactored to generic Quantity class

--- src/app/QuantityMeasurementApp.java
+++ src/app/QuantityMeasurementApp.java
@@ -2,28 +2,32 @@ package app;
 
 public class QuantityMeasurementApp {
 
-    static class Feet {
-        private final double value;
+    enum Unit {
+        FEET(1.0),
+        INCHES(1.0 / 12.0);
 
-        public Feet(double value) {
-            this.value = value;
+        private final double toFeet;
+
+        Unit(double toFeet) {
+            this.toFeet = toFeet;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || this.getClass() != obj.getClass()) return false;
-
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
+        public double toFeet(double value) {
+            return value * toFeet;
         }
     }
 
-    static class Inches {
+    static class Quantity {
         private final double value;
+        private final Unit unit;
 
-        public Inches(double value) {
+        public Quantity(double value, Unit unit) {
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
         }
 
         @Override
@@ -31,20 +35,17 @@ public class QuantityMeasurementApp {
             if (this == obj) return true;
             if (obj == null || this.getClass() != obj.getClass()) return false;
 
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
+            Quantity other = (Quantity) obj;
+
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
         }
     }
 
     public static void main(String[] args) {
 
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        Quantity q1 = new Quantity(1.0, Unit.FEET);
+        Quantity q2 = new Quantity(12.0, Unit.INCHES);
 
-        Inches i1 = new Inches(1.0);
-        Inches i2 = new Inches(1.0);
-
-        System.out.println("Feet Equal: " + f1.equals(f2));
-        System.out.println("Inches Equal: " + i1.equals(i2));
+        System.out.println("Equal: " + q1.equals(q2));
     }
 }
